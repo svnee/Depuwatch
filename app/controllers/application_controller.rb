df68b420d39562a8ccdc7beb673f34e978966340
Sven Clement <me@sven.lu>
2011-02-26 17:49:52 +0100

    Changed some static pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,22 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   helper_method :current_user_session, :current_user
 
+  def render_optional_error_file(status_code)
+    if status_code == :not_found
+      render_404
+    else
+      super
+    end
+  end
+  
+  def render_404
+    respond_to do |type| 
+      type.html { render :file => "public/404.html", :status => 404 } 
+      type.all  { render :nothing => true, :status => 404 } 
+    end
+    true  # so we can do "render_404 and return"
+  end
+
   private
     def current_user_session
       return @current_user_session if defined?(@current_user_session)
