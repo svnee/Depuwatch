999205a4cecc5e8de524f6f7738f1ea7291c2109
Sven Clement <me@sven.lu>
2011-02-26 17:22:10 +0100

    Capistrano update for sqlite

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,6 +11,8 @@ role :db,  "depuwatch.sven.lu", :primary => true # This is where Rails migration
 set :user, "chd"
 set :deploy_to, "/home/chd"
 set :use_sudo, false
+set :shared_database_path, "#{shared_path}/databases"
+set :shared_config_path, "#{shared_path}/configs"
 
 # If you are using Passenger mod_rails uncomment this:
 # if you're still using the script/reapear helper you will need
@@ -23,3 +25,30 @@ namespace :deploy do
     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
   end
 end
+
+namespace :sqlite3 do
+  desc "Generate a database configuration file"
+  task :build_configuration, :roles => :db do
+    db_options = {
+      "adapter"  => "sqlite3",
+      "database" => "#{shared_database_path}/chd.sqlite3"
+    }
+    config_options = {"production" => db_options}.to_yaml
+    put config_options, "#{shared_config_path}/sqlite_config.yml"
+  end
+ 
+  desc "Links the configuration file"
+  task :link_configuration_file, :roles => :db do
+    run "ln -nsf #{shared_config_path}/sqlite_config.yml #{release_path}/config/database.yml"
+  end
+ 
+  desc "Make a shared database folder"
+  task :make_shared_folder, :roles => :db do
+    run "mkdir -p #{shared_database_path}"
+  end
+end
+
+after "deploy:setup", "sqlite3:make_shared_folder"
+after "deploy:setup", "sqlite3:build_configuration"
+
+before "deploy:migrate", "sqlite3:link_configuration_file"
