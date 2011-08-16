4a30fd18ce85f31b33bf23ba6293d9b354156bb6
Sven Clement <git@sven.lu>
2011-08-16 14:18:56 +0200

    Playing with the save_votes method

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,19 +1,26 @@
 class AdminController < ApplicationController
   def vote
-  	@deputies = Deputy.all
+  	@deputies = Deputy.order(:name).all
   	@texts = Text.order(:code).all
   end
   
   def save_votes
     @text = Text.find(params[:text][:id])
-    
+    @text.votes.each { |v| v.destroy }
     params[:vote].each do |k,v|
-    	logger.info "#{k} -> #{v}"
+    	if (v != "noshow") then
+    		vote = 0
+    		case v
+    			when "no"
+    				vote = -1
+    			when "yes"
+    				vote = 1
+    			else
+    				vote = 0
+	    	end
+  			Vote.create({:text_id => @text.id, :deputy_id => k, :vote => vote, :delegate_id => params[:proxy][k]}).save!
+    	end
     end
-    #@text.votes.each { |v| v.destroy }
-    #params[:vote][:pour].each { |i| Vote.create({:text_id => @text.id, :deputy_id => i, :vote => 1}).save! }
-    #params[:vote][:contre].each { |i| Vote.create({:text_id => @text.id, :deputy_id => i, :vote => -1}).save! }
-    #params[:vote][:abstention].each { |i| Vote.create({:text_id => @text.id, :deputy_id => i, :vote => 0}).save! }
   	redirect_to @text
   end
 
