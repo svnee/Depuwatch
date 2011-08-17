da5c7f9d1ee52a67bd6562c2c3b839d120c4965d
Sven Clement <git@sven.lu>
2011-08-17 16:32:30 +0200

    relates to GH-43

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -49,10 +49,10 @@ class StaticController < ApplicationController
   
   def results_texts
   	qry = Array.new
-  	qry << "deputies.id = #{params[:deputy][:id]}" if (!params[:deputy][:id].nil?)
-  	qry << "deputies.party_id = #{params[:party][:id]}" if (!params[:party][:id].nil?)
-  	qry << "sessions.id = #{params[:session][:id]}" if (!params[:session][:id].nil?)
-  	qry << "topics.id = #{params[:topic][:id]}" if (!params[:topic][:id].nil?)
+  	qry << "deputies.id = #{params[:deputy][:id]}" if (!params[:deputy][:id].empty?)
+  	qry << "deputies.party_id = #{params[:party][:id]}" if (!params[:party][:id].empty?)
+  	qry << "sessions.id = #{params[:session][:id]}" if (!params[:session][:id].empty?)
+  	qry << "topics.id = #{params[:topic][:id]}" if (!params[:topic][:id].empty?)
   	qry << "votes.vote = 1" if (params[:vote] == "Jo")
   	qry << "votes.vote = -1" if (params[:vote] == "Nee")
   	qry << "votes.vote = 0" if (params[:vote] == "Abstentioun")
