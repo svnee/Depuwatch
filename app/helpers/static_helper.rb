8fdf0c58f98a06e937298cbb6b8445cced21eee2
Sven Clement <git@sven.lu>
2011-08-20 20:30:57 +0200

    first part of GH-43

--- app/helpers/static_helper.rb
+++ app/helpers/static_helper.rb
@@ -1,2 +1,22 @@
 module StaticHelper
+  def circonscription(bezierk)
+    if bezierk == 'all' then
+      'all'
+    else
+      case bezierk 
+        when 'N'
+          return 'Norden'
+        when 'Z'
+          return 'Zentrum'
+        when 'S'
+          return 'Süden'
+        when 'O' 
+          return "Osten"
+      end
+    end
+  end
+  
+  def session_display(s)
+    "#{s.start.strftime("%Y")}/#{s.end.strftime("%Y")}"
+  end
 end
