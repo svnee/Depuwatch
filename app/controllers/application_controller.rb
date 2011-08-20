5b85119332335269dd6187ff9c5b7948fab0553b
Sven Clement <git@sven.lu>
2011-08-20 21:50:07 +0200

    added a beta user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -34,7 +34,7 @@ class ApplicationController < ActionController::Base
   protected
     def authenticate
       authenticate_or_request_with_http_basic do |username, password|
-        username == "developer" && password == "depuwatch"
+        (username == "developer" && password == "depuwatch") || (username == "beta" && password == "ateb")
       end
     end
 end
