376b3a2b8e8fd993453e329229314bc7df264e3f
Sven Clement <git@sven.lu>
2011-09-10 21:36:27 +0200

    better update for the politician API

--- app/controllers/api/politicians_controller.rb
+++ app/controllers/api/politicians_controller.rb
@@ -6,7 +6,7 @@ class Api::PoliticiansController < ApplicationController
     
     respond_to do |format|
       format.html { render :text => "Please use the API only with application/xml", :status => :see_other }
-      format.xml  { render :xml => @politicians.to_xml(:only => [:id, :name, :start, :end, :pc_profile], :methods => [:partei, :bezierk, :active], :include => [:memberships, :votes], :skip_instruct => true, :root => 'politicians') }
+      format.xml  { render :xml => @politicians.to_xml(:only => [:id, :name, :start, :end, :pc_profile], :methods => [:partei, :bezierk, :active], :include => [:memberships], :skip_instruct => true, :root => 'politicians') }
       format.json { render :json => @politicians.to_json(:only => [:id, :name, :start, :end, :pc_profile], :methods => [:partei, :bezierk, :active], :include => [:memberships]).gsub!(/\\u([0-9a-z]{4})/) {|s| [$1.to_i(16)].pack("U")}}
     end
   end
@@ -16,7 +16,7 @@ class Api::PoliticiansController < ApplicationController
     
     respond_to do |format|
       format.html { render :text => "Please use the API only with application/xml", :status => :see_other }
-      format.xml  { render :xml => @politician.to_xml(:only => [:id, :name, :start, :end, :pc_profile], :methods => [:partei, :bezierk, :active], :include => [:memberships, :votes], :skip_instruct => true, :root => 'politician') }
+      format.xml  { render :xml => @politician.to_xml(:only => [:id, :name, :start, :end, :pc_profile], :methods => [:partei, :bezierk, :active, :votes], :include => [:memberships, :votes], :skip_instruct => true, :root => 'politician') }
       format.json { render :json => @politician.to_json(:only => [:id, :name, :start, :end, :pc_profile], :methods => [:partei, :bezierk, :active], :include => [:memberships], :skip_instruct => true, :root => 'politician').gsub!(/\\u([0-9a-z]{4})/) {|s| [$1.to_i(16)].pack("U")}}
     end
   end
