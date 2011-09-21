4ffb21e0eb1baa21a1e6c5d19f2de41a12815b1e
Sven Clement <git@sven.lu>
2011-09-21 21:58:17 +0200

    migration of servers

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,14 +4,14 @@ set :repository,  "git@github.com:svnee/Depuwatch.git"
 set :scm, :git
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
-role :web, "depuwatch.sven.lu"                          # Your HTTP server, Apache/etc
-role :app, "depuwatch.sven.lu"                          # This may be the same as your `Web` server
-role :db,  "depuwatch.sven.lu", :primary => true # This is where Rails migrations will run
+role :web, "85.93.211.139"                          # Your HTTP server, Apache/etc
+role :app, "85.93.211.139"                          # This may be the same as your `Web` server
+role :db,  "85.93.211.139", :primary => true        # This is where Rails migrations will run
 
-set :port, 5643
+set :port, 22
 
 set :user, "chd"
-set :deploy_to, "/home/chd"
+set :deploy_to, "/var/www"
 set :use_sudo, false
 set :shared_database_path, "#{shared_path}/databases"
 set :shared_config_path, "#{shared_path}/configs"
@@ -21,42 +21,15 @@ set :shared_config_path, "#{shared_path}/configs"
 # these http://github.com/rails/irs_process_scripts
 
 namespace :deploy do
-  task :start do ; end
-  task :stop do ; end
-  task :restart, :roles => :app, :except => { :no_release => true } do
-    run "ln -nsf /home/chd/shared/cache/ #{File.join(current_path,'tmp','cache')}"
-    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  desc "start unicorn"
+  task :start, :roles => :app, :except => {:no_release => true} do
+    run "cd #{current_path} && bundle exec unicorn_exec start"
   end
-  task :copy_old_sitemap do
-    run "if [ -e #{previous_release}/public/sitemap_index.xml.gz ]; then cp #{previous_release}/public/sitemap* #{current_release}/public/; fi"
+  desc "stop unicorn"
+  task :stop, :roles => :app, :except => {:no_release => true} do
+    run "#{current_path}/bundle exec unicorn_exec stop"
   end
-end
-
-namespace :sqlite3 do
-  desc "Generate a database configuration file"
-  task :build_configuration, :roles => :db do
-    db_options = {
-      "adapter"  => "sqlite3",
-      "database" => "#{shared_database_path}/chd.sqlite3"
-    }
-    config_options = {"production" => db_options}.to_yaml
-    put config_options, "#{shared_config_path}/sqlite_config.yml"
-  end
- 
-  desc "Links the configuration file"
-  task :link_configuration_file, :roles => :db do
-    run "ln -nsf #{shared_config_path}/sqlite_config.yml #{release_path}/config/database.yml"
-  end
- 
-  desc "Make a shared database folder"
-  task :make_shared_folder, :roles => :db do
-    run "mkdir -p #{shared_database_path}"
+  task :restart, :roles => :app, :except => {:no_release => true} do
+    ;
   end
 end
-
-after "deploy:setup", "sqlite3:make_shared_folder"
-after "deploy:setup", "sqlite3:build_configuration"
-
-before "deploy:migrate", "sqlite3:link_configuration_file"
-
-after "deploy:update_code", "deploy:copy_old_sitemap"
